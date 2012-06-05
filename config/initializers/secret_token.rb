eae3ee0292efa125fb610a40c3239c3b368058a4
Britton Ballard <davidbritton.ballard@gmail.com>
2012-06-04 21:55:52 -0500

    First commit of the eos project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eos::Application.config.secret_token = '938a585536d3eddcc7b8810387a07762a629da28feb46b123dd051c131184f92dd875fe121bdcab6af2fc9d9e2e13a4c3830b81312e94285da11f328183bbddc'
