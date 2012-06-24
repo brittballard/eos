cb657374d53d6fe14409ddbf08229201531a8b90
lemdj <douglemme@gmail.com>
2012-06-24 10:49:25 -0500

    adding login to the cucumber test for final lump sum calc

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Eos::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #Define default url options (required for devise)
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
